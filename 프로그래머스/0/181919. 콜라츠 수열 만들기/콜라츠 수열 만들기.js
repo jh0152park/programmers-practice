3643006b03701dca032ab87006d718070feb06ae
박재현 <jh0152park@naver.com>
2024-05-12 00:06:15 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.11 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [n];
+    
+    while(n !== 1) {
+        if(n % 2 === 0) {
+            answer.push(n / 2)
+            n /= 2;
+        } else {
+            answer.push((3 * n) + 1)
+            n *= 3
+            n++
+        }
+    }
+    
+    return answer;
+}
