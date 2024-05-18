a7dec5f694e71f11ea8f868f6f99bf691989a25d
박재현 <jh0152park@naver.com>
2024-05-18 20:47:08 +0900

    [level 0] Title: 약수 구하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [1];
+    
+    for(let i = 2; i <= n; i++) {
+        if(n % i === 0) {
+            answer.push(i)
+        }
+    }
+    
+    return answer;
+}
