e58a91c8d035e62d6b79cfcfc95968b0c9e1cc39
박재현 <jh0152park@naver.com>
2024-05-20 00:04:48 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -0,0 +1,8 @@
+function solution(my_string, n) {
+    var answer = '';
+    my_string.split("").forEach(c => {
+        for(let i = 0; i < n; i++)
+            answer += c;
+    })
+    return answer;
+}
