37d3365d7e13377bcabc40b4149dfa0e83b96e1c
박재현 <jh0152park@naver.com>
2024-06-25 00:05:24 +0900

    Update 베스트앨범.js

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.js
@@ -20,31 +20,6 @@ function solution(genres, plays) {
             results.push(Object.values(songInfo).indexOf(song));
         })
     })
-    
-    
-    
+       
     return results;
 }
-
-/*
-
-function solution(genres, plays) {
-
-  sortedGenre.forEach((genre) => {
-    // 장르별로 재생 횟수가 많은 노래 2개씩 추출
-    const genreSong = Object.values(info)
-      .filter((song) => song.genre === genre)
-      .sort((a, b) => b.play - a.play)
-      .slice(0, 2);
-
-    // 고유 번호를 result에 담음
-    genreSong.forEach((gs) => {
-      const uniqueId = Object.values(info).indexOf(gs);
-      result.push(uniqueId);
-    });
-  });
-
-  return result;
-}
-
-*/
