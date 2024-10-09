d6ea4b1a6368481f5109af25b0883614d5c981b7
박재현 <jh0152park@naver.com>
2024-10-09 18:33:37 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.py
@@ -0,0 +1,9 @@
+names = {
+    "NLCS": "North London Collegiate School",
+    "BHA": "Branksome Hall Asia",
+    "KIS": "Korea International School",
+    "SJA": "St. Johnsbury Academy",
+}
+
+name = input().strip()
+print(names[name])
