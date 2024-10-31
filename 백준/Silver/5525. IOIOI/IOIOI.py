765af014fa43d73a756a97cacf0aa24edb47d913
박재현 <jh0152park@naver.com>
2024-10-31 16:09:17 +0900

    [Silver I] Title: IOIOI, Time: 116 ms, Memory: 113132 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,26 +1,24 @@
 import sys
 
 
-def get_ioi(n):
-    str = ""
-
-    for i in range(n * 2 + 1):
-        if i % 2:
-            str += "O"
-        else:
-            str += "I"
-
-    return str
-
 n = int(sys.stdin.readline().strip())
-str_length = int(sys.stdin.readline().strip())
+m = int(sys.stdin.readline().strip())
 string = sys.stdin.readline().strip()
 
-cnt = 0
-ioi = get_ioi(n)
-ioi_len = len(ioi)
-for i in range(str_length - ioi_len + 1):
-    if string[i:i+ioi_len] == ioi:
-        cnt += 1
 
-print(cnt)
+i = 0
+detect = 0
+pattern = 0
+
+while i < m:
+    if string[i:i+3] == "IOI":
+        detect += 1
+        i += 2
+        if detect == n:
+            pattern += 1
+            detect -= 1
+    else:
+        i += 1
+        detect = 0
+
+print(pattern)
